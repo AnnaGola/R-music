cf5826c64d7bde489a2ca56656ef0555046cb484
Anna Goloshchapova <annagol.work@mail.ru>
2022-07-22 20:30:41 +0300

    delete storyboard

---
+++ R • music/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  R • music
+//
+//  Created by anna on 22.07.2022.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+}
