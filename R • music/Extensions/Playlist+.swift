ed5d0949c2882e39fd037fe8efdcde6df993cc01
Anna Goloshchapova <annagol.work@mail.ru>
2022-08-02 17:47:13 +0300

    extension for prev and next buttons in Playlist

---
+++ R • music/Extensions/Playlist+.swift
@@ -0,0 +1,44 @@
+//
+//  Playlist+.swift
+//  R • music
+//
+//  Created by anna on 02.08.2022.
+//
+
+import UIKit
+import SwiftUI
+
+extension Playlist: PlayAnotherSong {
+    
+    func playPrevSong() -> SearchViewModel.Cell? {
+
+        let index = songs.firstIndex(of: song)
+        guard let currentIndex = index else { return nil }
+        
+        var prevIndex: SearchViewModel.Cell
+        if currentIndex - 1 == -1 {
+            prevIndex = songs[songs.count - 1]
+        } else {
+            prevIndex = songs[currentIndex - 1]
+        }
+        self.song = prevIndex
+        return prevIndex
+    }
+    
+    func playNextSong() -> SearchViewModel.Cell? {
+        
+        let index = songs.firstIndex(of: song)
+        guard let currentIndex = index else { return nil }
+        
+        var nextIndex: SearchViewModel.Cell
+            if currentIndex + 1 == songs.count {
+                nextIndex = songs[0]
+            } else {
+                nextIndex = songs[currentIndex + 1]
+            }
+        
+        self.song = nextIndex
+        return nextIndex
+    }
+}
+
